219cf72975919fcd30542091aa2cb73b929f3d9f
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-11-21 22:21:04 +0900

    TCPソケット

---
+++ 6socket/keepclient/keepclient.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
+	}
+	current := 0
+	var conn net.Conn = nil
+	// リトライ用にループで全体を囲う
+	for {
+		var err error
+		// まだコネクションを張ってない / エラーでリトライ
+		if conn == nil {
+			conn, err = net.Dial("tcp", "localhost:8888")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
+		// POSTで文字列を送るリクエストを作成
+		request, err := http.NewRequest(
+			"POST",
+			"http://localhost:8888",
+			strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+		// サーバから読み込む。タイムアウトはここでエラーになるのでリトライ
+		response, err := http.ReadResponse(
+			bufio.NewReader(conn), request)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			panic(err)
+		}
+		// 結果を表示
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+	}
+	conn.Close()
+}
